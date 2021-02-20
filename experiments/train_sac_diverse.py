b6422e040cf46d9e96e5d6bcd9605a19346bb86f
Piotr Tarasiewicz <taras.piotr@gmail.com>
2021-02-20 15:54:02 +0000

    cleaning

--- experiments/train_sac_diverse.py
+++ experiments/train_sac_diverse.py
@@ -2,7 +2,7 @@
 import torch
 
 from grasp.sac.sac import sac
-from grasp.sac.core import CNNActorCritic, MLPActorCritic
+from grasp.sac.core import CNNActorCritic
 from grasp.env import KukaDiverseObjectEnv
 
 
@@ -23,15 +23,13 @@ def create_kuka_gym_diverse_env(test: bool = False):
 
 if __name__ == "__main__":
     print(f"CUDA IS{'' if torch.cuda.is_available() else ' NOT'} AVAILABLE!")
-
     seed = 0
 
-    # for alpha in (0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1, 2, 10, 100):
     sac(
         env_fn=create_kuka_gym_diverse_env,
         seed=seed,
         actor_critic=CNNActorCritic,
-        logger_kwargs={"output_dir": "/home/ucabpmt/data/", "exp_name": "sac_pixels",},
+        logger_kwargs={"exp_name": "sac_pixels",},
         replay_size=int(1e5),
         steps_per_epoch=1000,
         epochs=1000,
@@ -43,7 +41,6 @@ if __name__ == "__main__":
         max_ep_len=100,
         save_freq=1,
         gamma=0.95,
-        # ac_kwargs={"hidden_sizes": [16, 16]},
         alpha=1e-3,
         lr=1e-3,
     )
